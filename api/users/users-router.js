85dc2c08361bebd86b8a634213946e6567a84ea4
Jesus Navarrete <jesusnavarrete@Jesuss-MacBook-Pro.local>
2021-12-10 22:35:13 -0700

    notes deleted

--- api/users/users-router.js
+++ api/users/users-router.js
@@ -2,28 +2,7 @@
 const router = require('express').Router()
 const {restricted} = require('../auth/auth-middleware')
 const User = require('./users-model')
-/**
-  [GET] /api/users
 
-  This endpoint is RESTRICTED: only authenticated clients
-  should have access.
-
-  response:
-  status 200
-  [
-    {
-      "user_id": 1,
-      "username": "bob"
-    },
-    // etc
-  ]
-
-  response on non-authenticated:
-  status 401
-  {
-    "message": "You shall not pass!"
-  }
- */
 
   router.get('/', restricted, async (req, res, next) =>{
     try{
